89dccd15c48299be978ddb479d7569f384ef24eb
HDYS-TTBYS <tatehide151@outlook.jp>
2023-03-10 23:42:53 +0900

    TestTodoUseCase_FindById

--- go_api/usecase/todo_test.go
+++ go_api/usecase/todo_test.go
@@ -1,6 +1,7 @@
 package usecase_test
 
 import (
+	"net/http"
 	"testing"
 
 	"HDYS-TTBYS/my-todo/domain/entities"
@@ -8,6 +9,7 @@ import (
 	"HDYS-TTBYS/my-todo/usecase"
 
 	"github.com/golang/mock/gomock"
+	"github.com/labstack/echo"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -52,6 +54,46 @@ func TestTodoUseCase_FindMany(t *testing.T) {
 			todos, err := u.FindMany(0)
 			if assert.NoError(tt, err) {
 				assert.Equal(tt, &entities.ResponseTodos{Total: 1, ToDos: []*entities.ToDo{a}}, todos)
+			}
+		},
+	)
+}
+
+func TestTodoUseCase_FindById(t *testing.T) {
+	t.Run(
+		"データなし",
+		func(tt *testing.T) {
+			mockCtrl := gomock.NewController(t)
+			defer mockCtrl.Finish()
+			mockRepo := mock_repository.NewMockITodoRepository(mockCtrl)
+			mockRepo.EXPECT().FindById(1).Return(
+				nil,
+				echo.NewHTTPError(http.StatusInternalServerError, ""))
+			u := usecase.NewTodoUseCase(mockRepo)
+			_, err := u.FindById(1)
+			assert.Error(tt, err)
+		},
+	)
+	t.Run(
+		"データ1個あり",
+		func(tt *testing.T) {
+			mockCtrl := gomock.NewController(t)
+			defer mockCtrl.Finish()
+			mockRepo := mock_repository.NewMockITodoRepository(mockCtrl)
+			a := &entities.ToDo{
+				AssaginPerson: toPtr[string]("hdys"),
+				CreatedAt:     toPtr[int64](1),
+				Description:   toPtr[string]("description"),
+				Id:            toPtr[int](1),
+				IsComplete:    toPtr[bool](false),
+				Title:         "title",
+				UpdatedAt:     toPtr[int64](1),
+			}
+			mockRepo.EXPECT().FindById(1).Return(a, nil)
+			u := usecase.NewTodoUseCase(mockRepo)
+			todo, err := u.FindById(1)
+			if assert.NoError(tt, err) {
+				assert.Equal(tt, a, todo)
 			}
 		},
 	)
