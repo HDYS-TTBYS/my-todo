ebc957d4a76164a184666f23ede30f168a8898fb
HDYS-TTBYS <tatehide151@outlook.jp>
2023-03-10 22:01:04 +0900

    TestTodoRepository_Update

--- go_api/infrastructure/repository/todo_test.go
+++ go_api/infrastructure/repository/todo_test.go
@@ -5,7 +5,6 @@ import (
 	"HDYS-TTBYS/my-todo/ent/enttest"
 	"context"
 	"testing"
-	"time"
 
 	_ "github.com/mattn/go-sqlite3"
 	"github.com/stretchr/testify/assert"
@@ -21,7 +20,7 @@ func TestTodoRepository_TotalCount(t *testing.T) {
 			r := NewTodoRepository(client, c)
 			count, err := r.TotalCount()
 			if assert.NoError(tt, err) {
-				assert.Equal(tt, *count, 0)
+				assert.Equal(tt, 0, *count)
 			}
 		},
 	)
@@ -32,18 +31,15 @@ func TestTodoRepository_TotalCount(t *testing.T) {
 			defer client.Close()
 			c := context.Background()
 			r := NewTodoRepository(client, c)
-			now := time.Now()
 			_, err := client.Todo.Create().
 				SetAssaginPerson("hdys").
-				SetCreatedAt(now).
 				SetDescription("test description").
 				SetTitle("test title").
-				SetUpdatedAt(now).
 				Save(c)
 			if assert.NoError(ttt, err) {
 				count, err := r.TotalCount()
 				if assert.NoError(ttt, err) {
-					assert.Equal(ttt, *count, 1)
+					assert.Equal(ttt, 1, *count)
 				}
 			}
 		},
@@ -60,8 +56,8 @@ func TestTodoRepository_FindMany(t *testing.T) {
 			r := NewTodoRepository(client, c)
 			todos, err := r.FindMany(0)
 			if assert.NoError(tt, err) {
-				var t []*entities.ToDo
-				assert.Equal(tt, todos, t)
+				var expected []*entities.ToDo
+				assert.Equal(tt, expected, todos)
 			}
 		},
 	)
@@ -72,20 +68,17 @@ func TestTodoRepository_FindMany(t *testing.T) {
 			defer client.Close()
 			c := context.Background()
 			r := NewTodoRepository(client, c)
-			now := time.Now()
-			createdTodo, err := client.Todo.Create().
+			_, err := client.Todo.Create().
 				SetAssaginPerson("hdys").
-				SetCreatedAt(now).
 				SetDescription("test description").
 				SetTitle("test title").
-				SetUpdatedAt(now).
 				Save(c)
 			if assert.NoError(ttt, err) {
 				todos, err := r.FindMany(0)
 				if assert.NoError(ttt, err) {
-					var ttodo []*entities.ToDo
-					ttodo = append(ttodo, dataTransform(createdTodo))
-					assert.Equal(ttt, todos, ttodo)
+					assert.Equal(ttt, "hdys", *todos[0].AssaginPerson)
+					assert.Equal(ttt, "test description", *todos[0].Description)
+					assert.Equal(ttt, "test title", todos[0].Title)
 				}
 			}
 		},
@@ -113,18 +106,17 @@ func TestTodoRepository_FindById(t *testing.T) {
 			defer client.Close()
 			c := context.Background()
 			r := NewTodoRepository(client, c)
-			now := time.Now()
-			createdTodo, err := client.Todo.Create().
+			_, err := client.Todo.Create().
 				SetAssaginPerson("hdys").
-				SetCreatedAt(now).
 				SetDescription("test description").
 				SetTitle("test title").
-				SetUpdatedAt(now).
 				Save(c)
 			if assert.NoError(ttt, err) {
 				todo, err := r.FindById(1)
 				if assert.NoError(ttt, err) {
-					assert.Equal(ttt, todo, dataTransform(createdTodo))
+					assert.Equal(ttt, "hdys", *todo.AssaginPerson)
+					assert.Equal(ttt, "test description", *todo.Description)
+					assert.Equal(ttt, "test title", todo.Title)
 				}
 			}
 		},
@@ -144,8 +136,63 @@ func TestTodoRepository_Create(t *testing.T) {
 				AssiginPerson: "hdys",
 				Description:   &desc,
 				Title:         "test title"}
-			_, err := r.Create(postTodoJsonBody)
-			assert.NoError(tt, err)
+			createdTodo, err := r.Create(postTodoJsonBody)
+			if assert.NoError(tt, err) {
+				assert.Equal(tt, postTodoJsonBody.AssiginPerson, *createdTodo.AssaginPerson)
+				assert.Equal(tt, *postTodoJsonBody.Description, *createdTodo.Description)
+				assert.Equal(tt, postTodoJsonBody.Title, createdTodo.Title)
+			}
+		},
+	)
+}
+
+func TestTodoRepository_Update(t *testing.T) {
+	t.Run(
+		"データなし失敗",
+		func(tt *testing.T) {
+			client := enttest.Open(t, "sqlite3", "file:ent?mode=memory&_fk=1")
+			defer client.Close()
+			c := context.Background()
+			r := NewTodoRepository(client, c)
+			desc := "test description update"
+			updataTodoJsonBody := &entities.UpdateTodoIdJSONBody{
+				AssiginPerson: "hdys",
+				Description:   &desc,
+				IsComplete:    true,
+				Title:         "test title update",
+			}
+			_, err := r.Update(updataTodoJsonBody, 1)
+			assert.Error(tt, err)
+		},
+	)
+	t.Run(
+		"成功",
+		func(ttt *testing.T) {
+			client := enttest.Open(t, "sqlite3", "file:ent?mode=memory&_fk=1")
+			defer client.Close()
+			c := context.Background()
+			r := NewTodoRepository(client, c)
+			_, err := client.Todo.Create().
+				SetAssaginPerson("hdys").
+				SetDescription("test description").
+				SetTitle("test title").
+				Save(c)
+			if assert.NoError(ttt, err) {
+				desc := "test description update"
+				updataTodoJsonBody := &entities.UpdateTodoIdJSONBody{
+					AssiginPerson: "hdys",
+					Description:   &desc,
+					IsComplete:    true,
+					Title:         "test title update",
+				}
+				updatedTodo, err := r.Update(updataTodoJsonBody, 1)
+				if assert.NoError(ttt, err) {
+					assert.Equal(ttt, updataTodoJsonBody.AssiginPerson, *updatedTodo.AssaginPerson)
+					assert.Equal(ttt, *updataTodoJsonBody.Description, *updatedTodo.Description)
+					assert.Equal(ttt, updataTodoJsonBody.IsComplete, *updatedTodo.IsComplete)
+					assert.Equal(ttt, updataTodoJsonBody.Title, updatedTodo.Title)
+				}
+			}
 		},
 	)
 }
