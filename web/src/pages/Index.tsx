cc4a5968527426daacecb48a60de1b88f86ccf9c
HDYS-TTBYS <tatehide151@outlook.jp>
2023-03-19 02:05:14 +0900

    index

--- web/src/pages/Index.tsx
+++ web/src/pages/Index.tsx
@@ -2,7 +2,6 @@ import NavBar from '../components/NavBar';
 import { useQueryTodos } from '../hooks/useQueryTotos';
 import { BsFillTrash2Fill } from "@react-icons/all-files/bs/BsFillTrash2Fill";
 import { IconContext } from '@react-icons/all-files';
-import { time } from 'console';
 
 
 const Index = () => {
@@ -13,43 +12,51 @@ const Index = () => {
   return (
     <>
       <NavBar />
-      <div className='container'>
-        {JSON.stringify(data)}
-        <div className="my-3 p-3 bg-body rounded shadow-sm">
-          <h6 className="border-bottom pb-2 mb-0 fs-2">Todos</h6>
-
-          <div className="d-flex align-items-center text-muted pt-3 justify-content-between border-bottom">
-            <div className="pb-3 mb-0 small lh-sm w-100">
-              <div className="d-flex">
-                <strong className="text-gray-dark fs-4">Title</strong>
-              </div>
-              <span className="d-block fs-5">@assaginPerson:</span>
-            </div>
-
-            <div className="form-check form-switch d-flex align-items-center w-100 flex-column justify-content-center">
-              <input className="form-check-input d-flex" type="checkbox" id="flexSwitchCheckChecked" checked />
-              <label className="form-check-label d-flex" htmlFor="flexSwitchCheckChecked">completed</label>
-            </div>
-
-            <div className="pb-3 mb-0 small lh-sm w-100 d-flex flex-column justify-content-center">
-              <div className="d-flex">
-                created: {state.curDT}
-              </div>
-              <div className="d-flex">
-                updated: {state.curDT}
-              </div>
-            </div>
-
-            <div className='pb-3 mb-0 small lh-sm w-100 d-flex justify-content-center'>
-              <IconContext.Provider value={{ size: "50px" }}>
-                <BsFillTrash2Fill />
-              </IconContext.Provider>
-            </div>
-
+      <div className="container">
+        <div className="d-flex align-items-center p-3 my-3 text-white bg-purple rounded shadow-sm">
+          <div className="lh-1">
+            <h1 className="h6 mb-0 text-white lh-1">My Todo</h1>
+            <small>Since 2023</small>
           </div>
+        </div>
 
+        <div className="my-3 p-3 bg-body rounded shadow-sm">
+          <h6 className="border-bottom pb-2 mb-0">Todos</h6>
+          <div className="d-flex text-muted pt-3">
+
+            <div className="pb-3 mb-0 small lh-sm border-bottom w-100">
+              <div className="d-flex justify-content-between">
+                <strong className="text-gray-dark">Full Name</strong>
+
+                <div className="form-check form-switch d-flex">
+                  <input className="form-check-input" type="checkbox" id="flexSwitchCheckDefault" />
+                </div>
+
+                <div className='d-flex flex-column'>
+                  <div>
+                    {state.curDT}
+                  </div>
+                  <div>
+                    {state.curDT}
+                  </div>
+                </div>
+
+
+                <a href="#">
+                  <IconContext.Provider value={{ size: "20px" }}>
+                    <BsFillTrash2Fill />
+                  </IconContext.Provider>
+                </a>
+              </div>
+              <span className="d-block">@username</span>
+            </div>
+          </div>
+          <small className="d-block text-end mt-3">
+            <a href="#">All suggestions</a>
+          </small>
         </div>
       </div>
+
     </>
 
   )
